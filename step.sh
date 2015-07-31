4bda80ecc56390c5a633a232ba21312f35cf7c71
birmacher <birmacher@gmail.com>
2015-07-31 14:46:13 +0100

    added work directory support

--- step.sh
+++ step.sh
@@ -9,11 +9,19 @@ if [ -z "${fastlane_action}" ] ; then
   exit 1
 fi
 
+if [ -z "${work_dir}" ] ; then
+  echo "Missing required input: work_dir"
+  exit 1
+fi
+
 #
 # Install fastlane
 echo "Installing fastlane"
 gem install fastlane
+echo
 
 #
 # Running fastlane actions
+cd "${work_dir}"
+echo "Running action"
 fastlane "${fastlane_action}"
