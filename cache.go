fd381c37f18cfdeda68a199265a84c70f685e536
Gödrei Krisztián <godreikrisztian@gmail.com>
2019-09-02 23:02:27 +0200

    fastlane cache

--- cache.go
+++ cache.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 
+	"github.com/bitrise-io/bitrise-init/scanners/android"
+	"github.com/bitrise-io/bitrise-init/utility"
+	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/go-utils/pathutil"
 	androidCache "github.com/bitrise-steplib/bitrise-step-android-unit-test/cache"
 )
@@ -17,46 +21,97 @@ var depsFuncs = []depsFunc{
 }
 
 func cocoapodsDeps(dir string) ([]string, []string, error) {
-	iosDir, err := pathutil.AbsPath(filepath.Join(dir, "ios"))
+	files, err := utility.ListPathInDirSortedByComponents(dir, true)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to search for files in (%s), error: %s", dir, err)
+	}
+
+	locks, err := utility.FilterPaths(files, utility.BaseFilter("Podfile.lock", true))
 	if err != nil {
 		return nil, nil, err
 	}
 
-	podfileLockPth := filepath.Join(iosDir, "Podfile.lock")
-	if exist, err := pathutil.IsPathExists(podfileLockPth); err != nil {
-		return nil, nil, err
-	} else if !exist {
-		return nil, nil, nil
+	var relevant []string
+	for _, lock := range locks {
+		podsPth := filepath.Join(filepath.Dir(lock), "Pods")
+		exist, err := pathutil.IsPathExists(podsPth)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if exist {
+			relevant = append(relevant, lock)
+		}
 	}
 
-	return []string{fmt.Sprintf("%s -> %s", filepath.Join(iosDir, "Pods"), podfileLockPth)}, nil, nil
+	if len(relevant) > 0 {
+		log.Debugf("Multiple Podfile.lock found: %s", strings.Join(relevant, ", "))
+	}
+
+	var include []string
+	for _, lock := range locks {
+		podsPth := filepath.Join(filepath.Dir(lock), "Pods")
+		include = append(include, fmt.Sprintf("%s -> %s", podsPth, lock))
+	}
+
+	return include, nil, nil
 }
 
 func cacheCarthageDeps(dir string) ([]string, []string, error) {
-	iosDir, err := pathutil.AbsPath(filepath.Join(dir, "ios"))
+	files, err := utility.ListPathInDirSortedByComponents(dir, true)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to search for files in (%s), error: %s", dir, err)
+	}
+
+	locks, err := utility.FilterPaths(files, utility.BaseFilter("Cartfile.resolved", true))
 	if err != nil {
 		return nil, nil, err
 	}
 
-	cartfileResolvedPth := filepath.Join(iosDir, "Cartfile.resolved")
-	if exist, err := pathutil.IsPathExists(cartfileResolvedPth); err != nil {
-		return nil, nil, err
-	} else if !exist {
-		return nil, nil, nil
+	var relevant []string
+	for _, lock := range locks {
+		carthagePth := filepath.Join(filepath.Dir(lock), "Carthage")
+		exist, err := pathutil.IsPathExists(carthagePth)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if exist {
+			relevant = append(relevant, lock)
+		}
 	}
 
-	return []string{fmt.Sprintf("%s -> %s", filepath.Join(iosDir, "Carthage"), cartfileResolvedPth)}, nil, nil
+	if len(relevant) > 0 {
+		log.Debugf("Multiple Cartfile.resolved found: %s", strings.Join(relevant, ", "))
+	}
+
+	var include []string
+	for _, lock := range locks {
+		carthagePth := filepath.Join(filepath.Dir(lock), "Carthage")
+		include = append(include, fmt.Sprintf("%s -> %s", carthagePth, lock))
+	}
+
+	return include, nil, nil
 }
 
 func cacheAndroidDeps(dir string) ([]string, []string, error) {
-	androidDir := filepath.Join(dir, "android")
-
-	exist, err := pathutil.IsDirExists(androidDir)
+	scanner := android.NewScanner()
+	_, err := scanner.DetectPlatform(dir)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to check if directory (%s) exists, error: %s", androidDir, err)
+		return nil, nil, err
 	}
-	if !exist {
-		return nil, nil, nil
+
+	var include []string
+	var exclude []string
+	for _, dir := range scanner.ProjectRoots {
+		i, e, err := androidCache.Deps(dir, androidCache.LevelDeps)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		include = append(include, i...)
+		exclude = append(exclude, e...)
 	}
-	return androidCache.Deps(androidDir, androidCache.LevelDeps)
+
+	return include, exclude, err
 }
