e5a8db07daafb56abb15e93aa5a28787a9b52852
Gödrei Krisztián <godreikrisztian@gmail.com>
2019-09-03 15:17:51 +0200

    vendore deps

---
+++ vendor/github.com/bitrise-io/bitrise-init/scanners/android/utility.go
@@ -0,0 +1,190 @@
+package android
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/bitrise-io/bitrise-init/models"
+	"github.com/bitrise-io/bitrise-init/steps"
+	envmanModels "github.com/bitrise-io/envman/models"
+	"github.com/bitrise-io/go-utils/pathutil"
+)
+
+// Constants ...
+const (
+	ScannerName       = "android"
+	ConfigName        = "android-config"
+	DefaultConfigName = "default-android-config"
+
+	ProjectLocationInputKey     = "project_location"
+	ProjectLocationInputEnvKey  = "PROJECT_LOCATION"
+	ProjectLocationInputTitle   = "The root directory of an Android project"
+	ProjectLocationInputSummary = "The root directory of your Android project, stored as an Environment Variable. In your Workflows, you can specify paths relative to this path. You can change this at any time."
+
+	ModuleBuildGradlePathInputKey = "build_gradle_path"
+
+	VariantInputKey     = "variant"
+	VariantInputEnvKey  = "VARIANT"
+	VariantInputTitle   = "Variant"
+	VariantInputSummary = "Your Android build variant. You can add variants at any time, as well as further configure your existing variants later."
+
+	ModuleInputKey     = "module"
+	ModuleInputEnvKey  = "MODULE"
+	ModuleInputTitle   = "Module"
+	ModuleInputSummary = "Modules provide a container for your Android project's source code, resource files, and app level settings, such as the module-level build file and Android manifest file. Each module can be independently built, tested, and debugged. You can add new modules to your Bitrise builds at any time."
+
+	GradlewPathInputKey    = "gradlew_path"
+	GradlewPathInputEnvKey = "GRADLEW_PATH"
+	GradlewPathInputTitle  = "Gradlew file path"
+)
+
+func walk(src string, fn func(path string, info os.FileInfo) error) error {
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if path == src {
+			return nil
+		}
+		return fn(path, info)
+	})
+}
+
+func checkFiles(path string, files ...string) (bool, error) {
+	for _, file := range files {
+		exists, err := pathutil.IsPathExists(filepath.Join(path, file))
+		if err != nil {
+			return false, err
+		}
+		if !exists {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func walkMultipleFiles(searchDir string, files ...string) (matches []string, err error) {
+	match, err := checkFiles(searchDir, files...)
+	if err != nil {
+		return nil, err
+	}
+	if match {
+		matches = append(matches, searchDir)
+	}
+	return matches, walk(searchDir, func(path string, info os.FileInfo) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			match, err := checkFiles(path, files...)
+			if err != nil {
+				return err
+			}
+			if match {
+				matches = append(matches, path)
+			}
+		}
+		return nil
+	})
+}
+
+func checkGradlew(projectDir string) error {
+	gradlewPth := filepath.Join(projectDir, "gradlew")
+	exist, err := pathutil.IsPathExists(gradlewPth)
+	if err != nil {
+		return err
+	}
+	if !exist {
+		return errors.New(`<b>No Gradle Wrapper (gradlew) found.</b>
+Using a Gradle Wrapper (gradlew) is required, as the wrapper is what makes sure
+that the right Gradle version is installed and used for the build. More info/guide: <a>https://docs.gradle.org/current/userguide/gradle_wrapper.html</a>`)
+	}
+	return nil
+}
+
+func (scanner *Scanner) generateConfigBuilder() models.ConfigBuilderModel {
+	configBuilder := models.NewDefaultConfigBuilder()
+
+	projectLocationEnv, gradlewPath, moduleEnv, variantEnv := "$"+ProjectLocationInputEnvKey, "$"+ProjectLocationInputEnvKey+"/gradlew", "$"+ModuleInputEnvKey, "$"+VariantInputEnvKey
+
+	//-- primary
+	configBuilder.AppendStepListItemsTo(models.PrimaryWorkflowID, steps.DefaultPrepareStepList(true)...)
+	configBuilder.AppendStepListItemsTo(models.PrimaryWorkflowID, steps.InstallMissingAndroidToolsStepListItem(
+		envmanModels.EnvironmentItemModel{GradlewPathInputKey: gradlewPath},
+	))
+	configBuilder.AppendStepListItemsTo(models.PrimaryWorkflowID, steps.AndroidLintStepListItem(
+		envmanModels.EnvironmentItemModel{
+			ProjectLocationInputKey: projectLocationEnv,
+		},
+		envmanModels.EnvironmentItemModel{
+			ModuleInputKey: moduleEnv,
+		},
+		envmanModels.EnvironmentItemModel{
+			VariantInputKey: variantEnv,
+		},
+	))
+	configBuilder.AppendStepListItemsTo(models.PrimaryWorkflowID, steps.AndroidUnitTestStepListItem(
+		envmanModels.EnvironmentItemModel{
+			ProjectLocationInputKey: projectLocationEnv,
+		},
+		envmanModels.EnvironmentItemModel{
+			ModuleInputKey: moduleEnv,
+		},
+		envmanModels.EnvironmentItemModel{
+			VariantInputKey: variantEnv,
+		},
+	))
+	configBuilder.AppendStepListItemsTo(models.PrimaryWorkflowID, steps.DefaultDeployStepList(true)...)
+
+	//-- deploy
+	configBuilder.AppendStepListItemsTo(models.DeployWorkflowID, steps.DefaultPrepareStepList(true)...)
+	configBuilder.AppendStepListItemsTo(models.DeployWorkflowID, steps.InstallMissingAndroidToolsStepListItem(
+		envmanModels.EnvironmentItemModel{GradlewPathInputKey: gradlewPath},
+	))
+
+	configBuilder.AppendStepListItemsTo(models.DeployWorkflowID, steps.ChangeAndroidVersionCodeAndVersionNameStepListItem(
+		envmanModels.EnvironmentItemModel{ModuleBuildGradlePathInputKey: filepath.Join(projectLocationEnv, moduleEnv, "build.gradle")},
+	))
+
+	configBuilder.AppendStepListItemsTo(models.DeployWorkflowID, steps.AndroidLintStepListItem(
+		envmanModels.EnvironmentItemModel{
+			ProjectLocationInputKey: projectLocationEnv,
+		},
+		envmanModels.EnvironmentItemModel{
+			ModuleInputKey: moduleEnv,
+		},
+		envmanModels.EnvironmentItemModel{
+			VariantInputKey: variantEnv,
+		},
+	))
+	configBuilder.AppendStepListItemsTo(models.DeployWorkflowID, steps.AndroidUnitTestStepListItem(
+		envmanModels.EnvironmentItemModel{
+			ProjectLocationInputKey: projectLocationEnv,
+		},
+		envmanModels.EnvironmentItemModel{
+			ModuleInputKey: moduleEnv,
+		},
+		envmanModels.EnvironmentItemModel{
+			VariantInputKey: variantEnv,
+		},
+	))
+
+	configBuilder.AppendStepListItemsTo(models.DeployWorkflowID, steps.AndroidBuildStepListItem(
+		envmanModels.EnvironmentItemModel{
+			ProjectLocationInputKey: projectLocationEnv,
+		},
+		envmanModels.EnvironmentItemModel{
+			ModuleInputKey: moduleEnv,
+		},
+		envmanModels.EnvironmentItemModel{
+			VariantInputKey: variantEnv,
+		},
+	))
+	configBuilder.AppendStepListItemsTo(models.DeployWorkflowID, steps.SignAPKStepListItem())
+	configBuilder.AppendStepListItemsTo(models.DeployWorkflowID, steps.DefaultDeployStepList(true)...)
+
+	configBuilder.SetWorkflowDescriptionTo(models.DeployWorkflowID, deployWorkflowDescription)
+
+	return *configBuilder
+}
---
+++ vendor/github.com/bitrise-steplib/steps-deploy-to-itunesconnect-deliver/devportalservice/devportalservice.go
@@ -0,0 +1,177 @@
+package devportalservice
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/bitrise-io/go-utils/log"
+)
+
+// NetworkError represents a networking issue.
+type NetworkError struct {
+	Status int
+	Body   string
+}
+
+func (e NetworkError) Error() string {
+	return fmt.Sprintf("response %d %s", e.Status, e.Body)
+}
+
+// CIEnvMissingError represents an issue caused by missing environment variables,
+// which environment variables are exposed in builds on Bitrise.io.
+type CIEnvMissingError struct {
+	Key string
+}
+
+func (e CIEnvMissingError) Error() string {
+	return fmt.Sprintf("%s env is not exported", e.Key)
+}
+
+// portalData ...
+type portalData struct {
+	AppleID              string              `json:"apple_id"`
+	Password             string              `json:"password"`
+	ConnectionExpiryDate string              `json:"connection_expiry_date"`
+	SessionCookies       map[string][]cookie `json:"session_cookies"`
+}
+
+// cookie ...
+type cookie struct {
+	Name      string `json:"name"`
+	Path      string `json:"path"`
+	Value     string `json:"value"`
+	Domain    string `json:"domain"`
+	Secure    bool   `json:"secure"`
+	Expires   string `json:"expires,omitempty"`
+	MaxAge    int    `json:"max_age,omitempty"`
+	Httponly  bool   `json:"httponly"`
+	ForDomain *bool  `json:"for_domain,omitempty"`
+}
+
+// SessionData will fetch the session from Bitrise for the connected Apple developer account
+// If the BITRISE_PORTAL_DATA_JSON is provided (for debug purposes) it will use that instead.
+func SessionData() (string, error) {
+	p, err := getDeveloperPortalData(os.Getenv("BITRISE_BUILD_URL"), os.Getenv("BITRISE_BUILD_API_TOKEN"))
+	if err != nil {
+		return "", err
+	}
+
+	cookies, err := convertDesCookie(p.SessionCookies["https://idmsa.apple.com"])
+	if err != nil {
+		return "", err
+	}
+	return strings.Join(cookies, ""), nil
+}
+
+func getDeveloperPortalData(buildURL, buildAPIToken string) (portalData, error) {
+	var p portalData
+
+	j, exists := os.LookupEnv("BITRISE_PORTAL_DATA_JSON")
+	if exists && j != "" {
+		return p, json.Unmarshal([]byte(j), &p)
+	}
+
+	if buildURL == "" {
+		return portalData{}, CIEnvMissingError{Key: "BITRISE_BUILD_URL"}
+	}
+
+	if buildAPIToken == "" {
+		return portalData{}, CIEnvMissingError{Key: "BITRISE_BUILD_API_TOKEN env is not exported"}
+	}
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/apple_developer_portal_data.json", buildURL), nil)
+	if err != nil {
+		return portalData{}, err
+	}
+
+	req.Header.Add("BUILD_API_TOKEN", buildAPIToken)
+
+	if _, err := performRequest(req, &p); err != nil {
+		return portalData{}, err
+	}
+	return p, nil
+}
+
+func convertDesCookie(cookies []cookie) ([]string, error) {
+	var convertedCookies []string
+	var errs []string
+	for _, c := range cookies {
+		if convertedCookies == nil {
+			convertedCookies = append(convertedCookies, "---"+"\n")
+		}
+
+		if c.ForDomain == nil {
+			b := true
+			c.ForDomain = &b
+		}
+
+		tmpl, err := template.New("").Parse(`- !ruby/object:HTTP::Cookie
+  name: {{.Name}}
+  value: {{.Value}}
+  domain: {{.Domain}}
+  for_domain: {{.ForDomain}}
+  path: "{{.Path}}"
+`)
+		if err != nil {
+			errs = append(errs, fmt.Sprintf("Failed to create golang template for the cookie: %v", c))
+			continue
+		}
+
+		var b bytes.Buffer
+		err = tmpl.Execute(&b, c)
+		if err != nil {
+			errs = append(errs, fmt.Sprintf("Failed to parse cookie: %v", c))
+			continue
+		}
+
+		convertedCookies = append(convertedCookies, b.String()+"\n")
+	}
+
+	if len(errs) > 0 {
+		return nil, errors.New(strings.Join(errs, "\n"))
+	}
+	return convertedCookies, nil
+}
+
+func performRequest(req *http.Request, requestResponse interface{}) ([]byte, error) {
+	client := http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		// On error, any Response can be ignored
+		return nil, fmt.Errorf("failed to perform request, error: %s", err)
+	}
+
+	// The client must close the response body when finished with it
+	defer func() {
+		if cerr := response.Body.Close(); cerr != nil {
+			log.Warnf("Failed to close response body, error: %s", cerr)
+		}
+	}()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body, error: %s", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return nil, NetworkError{Status: response.StatusCode, Body: string(body)}
+	}
+
+	// Parse JSON body
+	if requestResponse != nil {
+		if err := json.Unmarshal([]byte(body), &requestResponse); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal response (%s), error: %s", body, err)
+		}
+	}
+	return body, nil
+}
+
+func main() {
+}
